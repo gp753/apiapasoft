ad30ec0d16a13d88e6e27b3e0c0dcfb411a513fb
hejasako <hejasako@PCZOTA>
2017-02-18 09:24:38 -0300

    5

--- api2/api2/Models/AccountViewModels.cs
+++ api2/api2/Models/AccountViewModels.cs
@@ -1,9 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 
+using System.ComponentModel.DataAnnotations;
+
 namespace api2.Models
 {
     // Models returned by AccountController actions.
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
 
     public class ExternalLoginViewModel
     {
